666c27cf9b3c5b3b06672cd0ffbb6529e174f41f
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-11-23 15:28:19 -0300

    conversao rebaixamento

---
+++ conversaorebaixamento.cpp
@@ -0,0 +1,21 @@
+//conversaoImplicita.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //promocao numerica
+    int v = 30;
+    float f = v;
+    cout << v/7<< endl;
+    cout << f/7<< endl;
+    //rebaixamento numerico
+
+    double n = 40.98;
+    int i = n;
+    cout << i<< endl;
+
+    return 0;
+}
