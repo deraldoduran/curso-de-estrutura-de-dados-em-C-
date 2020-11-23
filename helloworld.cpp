570b9b8b4da2ca993375709669c4dab187363cf2
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-11-23 13:09:56 -0300

    exercicio001

---
+++ helloworld.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+
+
+int main(){
+
+   std:: cout << "hello world!" << std::endl;
+
+   int z = 23;
+   std::cout<< "valor de z:" << z << std:: endl;
+
+   int k  (45.89);
+
+   std::cout<<"valor de k:" << k<<std::endl;
+
+   int s(23.75);
+   std::cout << "o valor de s:" << s<< std::endl;
+
+   int y {32};
+   std::cout<< "novo valor de y:"<< y << std::endl;
+
+   int x {88.12};
+   std::cout << "o valor de x: " << x << std::endl;
+
+    return 0;
+    }
