a57b13c3bc4e6826997e30cc00d03e838908a029
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-11-23 14:56:36 -0300

    exerciccio002

---
+++ funcaosizeof.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"bool :" << sizeof(bool) << "byte \n";
+    cout<<"char :"<< sizeof(char) << "byte \n";
+    cout << "int :" << sizeof(int) << "byte\n";
+    cout << "long :" << sizeof(long) <<"byte\n";
+    cout << "short int:" << sizeof(short int) <<"byte\n";
+    cout << "long int:" <<sizeof(long int) <<"byte\n";
+    cout << "long long:" <<sizeof(long long) <<"byte\n";
+    cout << "long unsigned int:" <<sizeof(long unsigned int) << "byte\n";
+
+
+    return 0;
+}
