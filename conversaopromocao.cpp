12e49a5ef520789ea00e34977a74afe9400446ea
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-11-23 15:27:30 -0300

    conversao promocao

---
+++ conversaopromocao.cpp
@@ -0,0 +1,15 @@
+//conversaoImplicita.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //promocao numerica
+    int v = 30;
+    float f = v;
+    cout << v/7<< endl;
+    cout << f/7<< endl;
+    return 0;
+}
