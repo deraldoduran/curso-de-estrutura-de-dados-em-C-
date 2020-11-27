543309af4f664df1e07d5cb71fda5575d61b292b
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-11-27 09:52:48 -0300

    funcionando

--- soldados.cpp
+++ soldados.cpp
@@ -1,54 +1,50 @@
 # include <iostream>
 using namespace std;
 
-int buscaNmaior(int **m, int l, int c){
-    int aux1 = 0;
-    int aux2 = 0;
-    int flag1 = 0;
-    int flag2 = 0;
-    int flag3 = 0;
-    for(int j =0; j<c-1; j++){
-        aux1 = m[1][j];
-        aux2 = aux1;
-        if(aux2<m[1][j+1]){
-            flag1 = 1;
 
-        }
-    }
-         aux1 = 0;
-         aux2 = 0;
-        for(int j =0; j<c-1; j++){
-        aux1 = m[2][j];
-        aux2 = aux1;
-        if(aux2<m[2][j+1]){
-            flag2 = 1;
-
-        }
-    }
-         aux1 = 0;
-         aux2 = 0;
-         for(int j =0; j<c-1; j++){
-        aux1 = m[3][j];
-        aux2 = aux1;
-        if(aux2<m[3][j+1]){
-            flag3 = 1;
-        }
-    }
-    int result = flag1+flag2+flag3;
-    return result;
-}
 
 
 int main(){
 int l = 3, c = 3;
+int vet[3];
+int soma= 0;
 int **mat = new int*[l];
 for (int i = 0; i<l; i++){
     int *aux = new int[c];
     mat[i] = aux;
     for(int j=0; j<c;j++){
         cin>>mat[i][j];
+        vet[i]=0;
     }
 }
-    cout<<buscaNmaior(mat,l,c);
+    //imprime matriz
+    for(int i=0;i<l;i++){
+        for(int j=0;j<c;j++){
+           // cout<<mat[i][j]<<"\t";
+            
+            //cout<<vet[i];
+        }
+    }
+
+    //comparacoes
+   for (int i=0; i<c ; i++) {
+        for (int j=l-2 ;j>-1; j-- ) {
+            for (int k=0; k<c ; k++ ) {
+                if(mat[k][i] > mat[j][i] and k<j){
+                    vet[i]=1;
+                }
+            }
+        }
+
+    }
+
+    //soma vetor
+    for(int i = 0; i<3;i++){
+        //cout<<vet[i];
+        soma+=vet[i];
+
+    }
+    cout<<soma;
+
     return 0;
 }
