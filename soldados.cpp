4577cbdcd5fb49a1dbe2652aec0307a74bf87367
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-11-27 19:08:12 -0300

    correto no moodle

--- soldados.cpp
+++ soldados.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
 int l = 3, c = 3;
-int vet[3];
+int vet[4] ={0,0,0,0};
 int soma= 0;
 int **mat = new int*[l];
 for (int i = 0; i<l; i++){
@@ -14,7 +14,7 @@ for (int i = 0; i<l; i++){
     mat[i] = aux;
     for(int j=0; j<c;j++){
         cin>>mat[i][j];
-        vet[i]=0;
+        //vet[i]=0;
     }
 }
     //imprime matriz
@@ -25,26 +25,28 @@ for (int i = 0; i<l; i++){
             //cout<<vet[i];
         }
     }
+    int cont = 0;
 
     //comparacoes
-   for (int i=0; i<c ; i++) {
-        for (int j=l-2 ;j>-1; j-- ) {
-            for (int k=0; k<c ; k++ ) {
-                if(mat[k][i] > mat[j][i] and k<j){
-                    vet[i]=1;
+   for (int i=0; i<l-2 ; i++) {
+                    for (int k=0; k<c ; k++ ) {
+                if(mat[i][k] > mat[i+1][k]){
+                    cont++;
+                }if(mat[i+1][k]>mat[i+2][k]){
+                    cont++;
                 }
             }
         }
 
-    }
+    
 
     //soma vetor
-    for(int i = 0; i<3;i++){
+    for(int i = 0; i<4;i++){
         //cout<<vet[i];
         soma+=vet[i];
 
     }
-    cout<<soma;
+    cout<<cont;
 
     return 0;
 }
