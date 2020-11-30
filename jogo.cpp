b045ec6c3d2e3702f0335b9703e17a28066b8f4f
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-11-30 18:13:36 -0300

    segunda versao

--- jogo.cpp
+++ jogo.cpp
@@ -1,30 +1,44 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
+struct JOGO {
+    double valorProdutos;
+    double chutes;
+    char segundo;
 
-string saida(double vp[], double c[], char s[],int tam){
+};
+
+void ler(struct JOGO j[], int t){
+    for (int i = 0; i<t;i++){
+        cin>>j[i].valorProdutos;
+        cin>>j[i].chutes;
+        cin>>j[i].segundo;
+    }
+}
+
+string saida(struct JOGO *j,int tam){
     string p = "primeiro";
     string e = "empate";
     string se = "segundo";
     double auxvp = 0;
     double auxc = 0;
     for(int i = 0; i<tam;i++){
-        auxvp = vp[i];
-        auxc = c[i];
-        if(vp[i]==c[i]){
+        auxvp = j[i].valorProdutos;
+        
+        if(j[i].valorProdutos==j[i].chutes){
             return p;
 
         }else{
-            if(c[i]<vp[i] and s[i]=='m'){
+            if(j[i].chutes<j[i].valorProdutos and j[i].segundo=='m'){
                 return e;
-            }else if(c[i]>vp[i] and s[i]=='M'){
+            }else if(j[i].chutes>j[i].valorProdutos and j[i].segundo=='M'){
                 return e;
-            }else if(s[i] == 'M' and c[i]<vp[i] and vp[i]<auxvp+1){
+            }else if(j[i].segundo == 'M' and j[i].chutes<j[i].valorProdutos and j[i].valorProdutos<auxvp+0.1){
                 return se;
-            }else if(s[i] == 'm' and c[i]>vp[i] and vp[i]<auxvp-1){
+            }else if(j[i].segundo == 'm' and j[i].chutes>j[i].valorProdutos and j[i].valorProdutos<auxvp-0.1){
                 return se;
-            }else if(s[i]=='M' and c[i]<vp[i] ){
+            }else if(j[i].segundo=='M' and j[i].chutes<j[i].valorProdutos ){
                 return p;
-            }else if(s[i] == 'm' and c[i]>vp[i]){
+            }else if(j[i].segundo == 'm' and j[i].chutes>j[i].valorProdutos){
                 return p;
             }
         }
@@ -33,20 +47,13 @@ string saida(double vp[], double c[], char s[],int tam){
 }
 
 int main(){
-    int const tam = 50;
-    int produtos[tam];
-    double valorProdutos[tam];
-    double chutes[tam];
-    char segundo[tam];
-    int qtd = 0;
-    cin>>qtd;
 
-    for(int i = 0; i<qtd; i++){
-        cin>>valorProdutos[i];
-        cin>>chutes[i];
-        cin>>segundo[i];
-    }
-    cout<<saida(valorProdutos,chutes,segundo, tam)<<std::endl;
+int jogadores;
+cin>>jogadores;
+
+struct JOGO individuo[jogadores];
+ler(individuo, jogadores);
+cout<<saida(individuo,jogadores)<<std::endl;
 
     return 0;
 }
