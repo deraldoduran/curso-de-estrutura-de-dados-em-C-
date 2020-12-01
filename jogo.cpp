d34436695dec39c918ac14ac3ab23335dc44089a
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-12-01 19:04:31 -0300

    funciona

--- jogo.cpp
+++ jogo.cpp
@@ -1,59 +1,62 @@
 #include <iostream>
+
 using namespace std;
-struct JOGO {
-    double valorProdutos;
-    double chutes;
-    char segundo;
 
-};
 
-void ler(struct JOGO j[], int t){
-    for (int i = 0; i<t;i++){
-        cin>>j[i].valorProdutos;
-        cin>>j[i].chutes;
-        cin>>j[i].segundo;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    int player1 = 0;
+    int player2 = 0;
+    
+    cin >> a;
+
+    float produtos[a];
+    float chutesP[a];
+    char escolhaS[a];
+
+
+    for(int i = 0; i < a; i++){
+        cin >> produtos[i];
+        cin.ignore();
     }
-}
+    for(int i = 0; i < a; i++){
+        cin >> chutesP[i];
+        cin.ignore();
+    }
+    
+    for(int i = 0; i < a; i++){
+        cin >> escolhaS[i];
+        cin.ignore();
 
-string saida(struct JOGO *j,int tam){
-    string p = "primeiro";
-    string e = "empate";
-    string se = "segundo";
-    double auxvp = 0;
-    double auxc = 0;
-    for(int i = 0; i<tam;i++){
-        auxvp = j[i].valorProdutos;
+    }
+
+    for(int i = 0; i < a; i++){
         
-        if(j[i].valorProdutos==j[i].chutes){
-            return p;
-
-        }else{
-            if(j[i].chutes<j[i].valorProdutos and j[i].segundo=='m'){
-                return e;
-            }else if(j[i].chutes>j[i].valorProdutos and j[i].segundo=='M'){
-                return e;
-            }else if(j[i].segundo == 'M' and j[i].chutes<j[i].valorProdutos and j[i].valorProdutos<auxvp+0.1){
-                return se;
-            }else if(j[i].segundo == 'm' and j[i].chutes>j[i].valorProdutos and j[i].valorProdutos<auxvp-0.1){
-                return se;
-            }else if(j[i].segundo=='M' and j[i].chutes<j[i].valorProdutos ){
-                return p;
-            }else if(j[i].segundo == 'm' and j[i].chutes>j[i].valorProdutos){
-                return p;
-            }
+        if( (produtos[i] == chutesP[i])||
+            (produtos[i] > chutesP[i] && escolhaS[i] == 'm')||
+            (produtos[i] < chutesP[i] && escolhaS[i] == 'M')
+            ){
+                player1++;
+        }else
+        {
+            player2++;
         }
+            
     }
-    return p;
-}
+    if (player1 == player2)
+    {
+        cout << "empate"<< endl;
+    }else if (player1 > player2)
+    {
+        cout << "primeiro"<< endl;
+    }else{
+        cout << "segundo"<< endl;
+    }
+    
+    
 
-int main(){
-
-int jogadores;
-cin>>jogadores;
-
-struct JOGO individuo[jogadores];
-ler(individuo, jogadores);
-cout<<saida(individuo,jogadores)<<std::endl;
 
     return 0;
 }
