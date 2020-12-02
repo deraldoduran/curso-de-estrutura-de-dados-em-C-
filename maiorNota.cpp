f4c397a30dcb47e2a7f164cf64c803b87d8ac927
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-12-02 15:39:41 -0300

    ta cortando a primeira letra

--- maiorNota.cpp
+++ maiorNota.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include<string>
 using namespace std;
 
 struct Aluno
@@ -6,31 +8,36 @@ struct Aluno
     string nome;
     int matricula;
     string disciplina;
-    double nota;
+    float nota;
 };
 void ler(Aluno *a, int t){
     
     for(int i=0;i<t;i++){
-        cin>>a[i].nome;
-         cin.ignore();
+       
+          cin.ignore();
+        std::getline (std::cin,a[i].nome);
+        
+
         cin>>a[i].matricula;
+         
         
         cin.ignore();
         cin>>a[i].disciplina;
-         cin.ignore();
+         
         cin>>a[i].nota;
-        cin.ignore();
+        
     }
 }
 
 void compara(Aluno *a, int t){
-    if(a[1].nota == a[2].nota){
-        cout<<a[1].nome<<"e"<<a[2].nome<<a[1].nota<<std::endl;
-    }else if(a[2].nota>a[1].nota){
-        cout<<a[2].nome<<","<<a[2].nota<< endl;
-    }else if(a[1].nota>a[2].nota){
-        std::cout<<a[1].nome<<","<<a[1].nota<< endl;
-    }
+    if(a[0].nota == a[1].nota){
+        cout<<a[0].nome<<" "<<"e"<<" "<<a[1].nome<<std::fixed<<std::setprecision(1)<<a[1].nota<<std::endl;
+    }else if(a[1].nota>a[0].nota){
+        cout<<a[1].nome<<""<<" , "<<""<<std::fixed<<std::setprecision(1)<<a[1].nota<< endl;
+    }else if(a[0].nota>a[1].nota) {
+        std::cout<<a[0].nome<<""<<" , "<<""<<std::fixed<<std::setprecision(1)<<a[0].nota<< endl;
+    
+}
 }
 
 int main(){
@@ -38,10 +45,7 @@ int main(){
    // cin>>size;
     Aluno alunos[size];
     ler(alunos, size);
-    
-        cout<<alunos[1].nota<<"nota 1"<<alunos[2].nota<<"aluno 2"<<endl;
-    
-    
+    //saida(alunos, size);
     compara(alunos, size);
   
     return 0;
