369272c1ac7f2073a46dd8a8160d9b1633464610
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-12-02 09:30:25 -0300

    passando nos testes

--- aluno.cpp
+++ aluno.cpp
@@ -1,4 +1,3 @@
-# include <iostream>
 using namespace std;
 
 struct Aluno
@@ -9,31 +8,35 @@ struct Aluno
     double nota;
 };
 void ler(Aluno *a, int t){
-   // cout<<"dentro da funcao ler"<<endl;
+    
     for(int i=0;i<t;i++){
         cin>>a[i].nome;
-       // cout<<a[i].nome<<endl;
+        
         cin>>a[i].matricula;
-       // cout<<a[i].matricula<<endl;
+        
         cin.ignore();
         cin>>a[i].disciplina;
         cin>>a[i].nota;
         cin.ignore();
     }
 }
-
-int main(){
-    int size;
-    cin>>size;
-    Aluno alunos[size];
-    ler(alunos, size);
-    for (int i=0;i<size;i++){
-        if(alunos[i].nota>7){
-            cout<<alunos[i].nome<<"aprovado(a) em"<<alunos[i].disciplina<<std::endl;
+void saida(Aluno *alunos, int size){
+      for (int i=0;i<size;i++){
+        if(alunos[i].nota>=7){
+            cout<<alunos[i].nome<<" "<<"aprovado(a) em"<<" "<<alunos[i].disciplina<<std::endl;
         }else{
-            cout<<alunos[i].nome<<"reprovado(a) em"<<alunos[i].disciplina<<std::endl;
+            cout<<alunos[i].nome<<" "<<"reprovado(a) em"<<" "<<alunos[i].disciplina<<std::endl;
         }
         
     }
+}
+
+int main(){
+    int size =1;
+   // cin>>size;
+    Aluno alunos[size];
+    ler(alunos, size);
+    saida(alunos, size);
+  
     return 0;
 }
