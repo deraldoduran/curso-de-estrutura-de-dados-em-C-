4e9462fca809581d5f38b54987be11408b85f4c1
Duran <42320816+deraldoduran@users.noreply.github.com>
2020-12-02 09:17:51 -0300

    nao imprime os dados

--- aluno.cpp
+++ aluno.cpp
@@ -9,9 +9,12 @@ struct Aluno
     double nota;
 };
 void ler(Aluno *a, int t){
+   // cout<<"dentro da funcao ler"<<endl;
     for(int i=0;i<t;i++){
         cin>>a[i].nome;
+       // cout<<a[i].nome<<endl;
         cin>>a[i].matricula;
+       // cout<<a[i].matricula<<endl;
         cin.ignore();
         cin>>a[i].disciplina;
         cin>>a[i].nota;
@@ -24,6 +27,13 @@ int main(){
     cin>>size;
     Aluno alunos[size];
     ler(alunos, size);
-
+    for (int i=0;i<size;i++){
+        if(alunos[i].nota>7){
+            cout<<alunos[i].nome<<"aprovado(a) em"<<alunos[i].disciplina<<std::endl;
+        }else{
+            cout<<alunos[i].nome<<"reprovado(a) em"<<alunos[i].disciplina<<std::endl;
+        }
+        
+    }
     return 0;
 }
